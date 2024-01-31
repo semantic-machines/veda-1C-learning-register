9b34c4e4c715f8b59f701e4017144c5066c66bb3
Roman Karpov <roman.karpov@gmail.com>
2024-01-31 23:10:32 +0300

    upd

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 time=`date -Iseconds`
 echo $time
-/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node src/main.js >> $PWD/${PWD##*/}-$time.log 2>&1"
+/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node --use-openssl-ca src/veda-1c.js >> $PWD/${PWD##*/}-$time.log 2>&1"
